6f392b9beff5efddb9a770f3f059ccca798021dc
Charles Yang Zheng <snarles@celadon>
2014-07-29 23:37:26 -0700

    manual syllables

--- processnames.py
+++ processnames.py
@@ -8,12 +8,14 @@ import string
 
 # get raw names
 rawns = {}
-splitns = {}
-listns = {}
-count = 0
 for ii in range(len(ff)-1):
     ss = ff[ii]
     if (ss[0]=='*'):
-        sss = ss.replace('-',' ').split(' ')[2:]
+        sss = ss.split(' ')[2:]
         ss2 = ''.join(sss)
+        rawns[ss2]=1
+ns = rawns.keys()
 
+
+o = open('p_cardlist.txt','w')
+f.write('\n'.join(ns))
