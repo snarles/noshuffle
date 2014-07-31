3860f2bcf3b40614487bd0e834b14c58d7421ce3
Charles Yang Zheng <snarles@celadon>
2014-07-31 14:38:44 -0700

    generating abbrevs

--- gen_abbrevs.py
+++ gen_abbrevs.py
@@ -3,3 +3,12 @@
 f = open('p_cardlist.txt','r')
 listns = f.read().split('\n')
 f.close()
+
+f = open('annotated_cardlist.txt','r')
+splitns = f.read().split('\n')
+f.close()
+for ii in range(len(splitns)):
+    splitns[ii] = splitns[ii].split(' ')
+    for jj in range(len(splitns[ii])):
+        splitns[ii][jj] = splitns[ii][jj].split('.')
+
