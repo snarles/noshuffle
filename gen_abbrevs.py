ba0f5f7fdf1f635aa48fafc024c2c3af0eb7ebf4
Charles Yang Zheng <snarles@celadon>
2014-08-11 19:01:51 -0700

    fixed stuff

--- gen_abbrevs.py
+++ gen_abbrevs.py
@@ -8,7 +8,7 @@ f = open('annotated_cardlist.txt','r')
 splitns = f.read().split('\n')
 f.close()
 for ii in range(len(splitns)):
-    splitns[ii] = splitns[ii].split(' ')
+    splitns[ii] = splitns[ii].upper().replace('-',' ').split(' ')
     for jj in range(len(splitns[ii])):
         splitns[ii][jj] = splitns[ii][jj].split('.')
-
+splitns = splitns[:len(listns)]
